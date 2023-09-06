4610f446d5bbbf6c7e4faebabd3ddfd345891124
a01177702 <a01177702@tec.mx>
2023-09-06 13:58:28 -0600

    asi lo tenia barajas ya no se

--- interface/src/components/establish-connection/EstablishConnection.jsx
+++ interface/src/components/establish-connection/EstablishConnection.jsx
@@ -1,37 +1,31 @@
-// EstablishConnection.jsx
-import React, { useContext } from "react";
+import { useState, useContext } from "react";
 import { AppContext } from "../../context/AppContext.jsx";
 
+
 export default function EstablishConnection() {
-  const { ws, id, unityId, setUnityId, setStep } = useContext(AppContext);
+  const { ws, id, unityId, setUnityId } = useContext(AppContext);
 
   function handleSubmit(e) {
     e.preventDefault();
     if (ws && ws.readyState === WebSocket.OPEN) {
-      ws.send(
-        JSON.stringify({
-          type: "connect",
-          sender: id,
-          receiver: unityId,
-          data: unityId,
-        })
-      );
+      ws.send(JSON.stringify({
+        type: "connect",
+        sender: id,
+        receiver: unityId,
+        data: unityId,
+      }));
     } else {
       console.log(ws);
       console.log("WebSocket connection not ready.");
     }
   }
 
-  function handleConnectionSuccess() {
-    // Handle the logic when the connection is successful
-    // For example, change the step to move to the BeforeSimulationPage
-    setStep(2); // Assuming 2 represents BeforeSimulationPage
-  }
-
   return (
     <div className="flex flex-col justify-center items-center bg-white h-full">
       <div className="w-1/4 bg-gray-50 rounded shadow p-6 flex items-center mb-6">
-        <form onSubmit={handleSubmit}>
+        <form
+          onSubmit={handleSubmit}
+        >
           <label className="block text-base mb-1">ID de conexión</label>
           <input
             className="border-2 border-gray-300 rounded p-1 w-full"
@@ -44,12 +38,13 @@ export default function EstablishConnection() {
           <button
             className="bg-[#367c2b] text-white rounded p-1 mt-4 w-full"
             type="submit"
-            onClick={handleConnectionSuccess}
           >
             Conectar
           </button>
         </form>
       </div>
     </div>
-  );
+
+
+  )
 }
--- interface/src/routes/Routes.jsx
+++ interface/src/routes/Routes.jsx
@@ -1,5 +1,4 @@
-// Routes.jsx
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext.jsx";
 import EstablishConnection from "../components/establish-connection/EstablishConnection.jsx";
 import BeforeSimulationPage from "../components/BeforeSimulationPage.jsx";
@@ -8,14 +7,9 @@ import AfterSimulationPage from "../components/AfterSimulationPage.jsx";
 import JohnDeere_Logo from "../public/John_Deere_logo.svg.png";
 
 export default function Routes() {
-  const { step, ws, setStep } = useContext(AppContext);
-
-  useEffect(() => {
-    if (step === 2 && ws && ws.readyState === WebSocket.OPEN) {
-      // If step is 2 and WebSocket connection is open, transition to BeforeSimulationPage
-      setStep(2); // Assuming 1 represents BeforeSimulationPage
-    }
-  }, [step, ws, setStep]);
+  const { step } = useContext(AppContext);
+  // const step = 2;
+  console.log(step);
 
   return (
     <div className="h-screen">
@@ -23,12 +17,23 @@ export default function Routes() {
         <div className="w-[30px] h-full bg-[#20720D] text-[#20720D]">asdf</div>
         <div className="w-[10px] h-full bg-[#FDE100] text-[#FDE100]"></div>
         <div className="flex flex-row items-baseline ml-6">
+
           <img src={JohnDeere_Logo} width={100} height={50} alt="" />
           <h1 className="ml-5 text-2xl text-black font-trebuchet-ms">John Deere Simulation</h1>
         </div>
       </header>
       <div className="h-5/6">
-        {step === 0 ? <EstablishConnection /> : step === 1 ? <BeforeSimulationPage /> : step === 2 ? <DuringSimulationPage /> : step === 3 ? <AfterSimulationPage /> : null}
+        {step === 0 ?
+          <EstablishConnection />
+          : step === 1 ?
+            <DuringSimulationPage />
+            : step === 2 ?
+              <BeforeSimulationPage />
+              : step === 3 ?
+                <AfterSimulationPage />
+                :
+                null
+        }
       </div>
     </div>
   );
